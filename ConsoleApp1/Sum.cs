08a11bdcd586f9588f950dc4d2f6b52649d7f71b
raisircl <rai.verma@live.com>
2026-04-01 11:51:44 +0530

    Add project files.

---
+++ ConsoleApp1/Sum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Sum
+    {
+        static void Main()
+        {
+            int n1, n2, total;
+            
+            Console.WriteLine("enter N1");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter N2");
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            total = n1 + n2;
+            Console.WriteLine($"The Sum of {n1} and {n2} is {total}");
+        }
+    }
+}
