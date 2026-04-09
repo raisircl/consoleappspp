4efee3184f53254f8dab7d3b9c5857761c263b3b
raisircl <rai.verma@live.com>
2026-04-09 12:53:44 +0530

    array

--- ConsoleApp1/Functions.cs
+++ ConsoleApp1/Functions.cs
@@ -1,73 +1,73 @@
-﻿using Microsoft.VisualBasic;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using Microsoft.VisualBasic;
+//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace ConsoleApp1
-{
-    //call by value
-    //call by reference
-    //call by out
+//namespace ConsoleApp1
+//{
+//    //call by value
+//    //call by reference
+//    //call by out
 
-    internal class Functions
-    {
-        static void Main()
-        {
-            msg();
-            table(5);   
-            Console.WriteLine($"Factorial of 5 is {factorial(5)}");
-            Console.WriteLine($"Value of pi is {getpi()}");
+//    internal class Functions
+//    {
+//        static void Main()
+//        {
+//            msg();
+//            table(5);   
+//            Console.WriteLine($"Factorial of 5 is {factorial(5)}");
+//            Console.WriteLine($"Value of pi is {getpi()}");
 
-            int x = 10, y = 20; // actual parameters
-            Console.WriteLine($"Before swap: x = {x}, y = {y}");
-                //swap(x, y);
-                swapref(ref x, ref y);
-            Console.WriteLine($"After swap: x = {x}, y = {y}");
-            int hindi = 85, eng = 90, math = 95, sci = 80, sst = 88;
-            int total;
-            double p;
-            Result(hindi, eng, math, sci, sst, out total, out p);
-            Console.WriteLine($"Total Marks: {total}, Percentage: {p}%");
-        }
-        static void swap(int a, int b) // call by value - a and b formal parameters
-        {
-            int temp = a;  // in call by value, changes to a and b do not affect x and y in Main
-            a = b;
-            b = temp;
-        }
-        static void swapref(ref int a, ref int b) // call by reference - a and b formal parameters
-        {
-            int temp = a;  // in call by reference, changes to a and b will affect x and y in Main
-            a = b;
-            b = temp;
-        }
-        //call by out is used when we want to return multiple values from a method. It allows us to pass parameters that will be assigned values within the method and returned to the caller.
-        static void Result(int hindi, int eng, int math, int sci, int sst, out int total, out double percentage)
-        {
-            total = hindi + eng + math + sci + sst;
-            percentage = (total / 500.0) * 100;
-        }
-        static void msg()
-        {
-            Console.WriteLine("Hello World");
-        }
-        static void table(int num)
-        {
-            for (int i = 1; i <= 10; i++)
-            {
-                Console.WriteLine($"{num} x {i} = {num * i}");
-            }
-        }
-        static int factorial(int n)
-        {
-            if (n == 0)
-                return 1;
-            else
-                return n * factorial(n - 1);
-        }
-        static double getpi()
-        {
-            return 3.14;
-        }
-    }
-}
+//            int x = 10, y = 20; // actual parameters
+//            Console.WriteLine($"Before swap: x = {x}, y = {y}");
+//                //swap(x, y);
+//                swapref(ref x, ref y);
+//            Console.WriteLine($"After swap: x = {x}, y = {y}");
+//            int hindi = 85, eng = 90, math = 95, sci = 80, sst = 88;
+//            int total;
+//            double p;
+//            Result(hindi, eng, math, sci, sst, out total, out p);
+//            Console.WriteLine($"Total Marks: {total}, Percentage: {p}%");
+//        }
+//        static void swap(int a, int b) // call by value - a and b formal parameters
+//        {
+//            int temp = a;  // in call by value, changes to a and b do not affect x and y in Main
+//            a = b;
+//            b = temp;
+//        }
+//        static void swapref(ref int a, ref int b) // call by reference - a and b formal parameters
+//        {
+//            int temp = a;  // in call by reference, changes to a and b will affect x and y in Main
+//            a = b;
+//            b = temp;
+//        }
+//        //call by out is used when we want to return multiple values from a method. It allows us to pass parameters that will be assigned values within the method and returned to the caller.
+//        static void Result(int hindi, int eng, int math, int sci, int sst, out int total, out double percentage)
+//        {
+//            total = hindi + eng + math + sci + sst;
+//            percentage = (total / 500.0) * 100;
+//        }
+//        static void msg()
+//        {
+//            Console.WriteLine("Hello World");
+//        }
+//        static void table(int num)
+//        {
+//            for (int i = 1; i <= 10; i++)
+//            {
+//                Console.WriteLine($"{num} x {i} = {num * i}");
+//            }
+//        }
+//        static int factorial(int n)
+//        {
+//            if (n == 0)
+//                return 1;
+//            else
+//                return n * factorial(n - 1);
+//        }
+//        static double getpi()
+//        {
+//            return 3.14;
+//        }
+//    }
+//}
---
+++ ConsoleApp1/SDArray.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class SDArray
+    {
+        // Array collection of items of same type of data
+        // Syntax: dataType[] arrayName = new dataType[size];
+        static void Main()
+        {
+            int[] a = new int[5]; // declaration and instantiation of array
+            int i;
+
+            /*a[0] = 100; // initialization of array
+            a[1] = 200;
+            a[2] = 300;
+            a[3] = 400;
+            a[4] = 500;
+            */
+            for (i = 0; i < 5; i++) // last element of array is always size-1
+            {
+                Console.WriteLine("Enter element {0} of array:", i);
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            // Accessing array elements
+            /*
+            Console.WriteLine(a[0]);
+            Console.WriteLine(a[1]);
+            Console.WriteLine(a[2]);
+            Console.WriteLine(a[3]);
+            Console.WriteLine(a[4]);
+            */
+            Console.WriteLine("Elements of array are:");
+            int x = 0;
+            int y = 0;
+
+            for (i = 0; i < 5; i++)
+            {
+                x = x + a[i];
+                Console.WriteLine(a[i]);
+                if (y < a[i])
+                {
+                    y = a[i];
+                }
+            }
+            Console.WriteLine("Sum of array elements is: {0}", x);
+            Console.WriteLine("Largest element of array is: {0}", y);
+
+
+
+            int z;
+            Console.WriteLine("Enter element to search in array:");
+            z = Convert.ToInt32(Console.ReadLine());
+            bool found = false;
+            for (i = 0; i < a.Length; i++)
+            {
+                if (z == a[i])
+                {
+                    found = true;
+                    break;
+                }
+            }
+            if (found)
+            {
+                Console.WriteLine($"Element {z} is found at {i} in array.");
+            }
+            else
+            {
+                Console.WriteLine("Element {0} is not found in array.", z);
+            }
+            int temp;
+            for (i = 0; i < a.Length; i++)
+            {
+                for (int j = i + 1; j < a.Length; j++)
+                {
+                    if (a[i] > a[j])
+                    {
+                        temp = a[i];
+                        a[i] = a[j];
+                        a[j] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Sorted array is:");
+            for (i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+        }
+    }
+}
+// search an element in arrant and count number of times it is present in array
+//Reverse an Array: Print elements in reverse order or physically reverse their positions in the array.
+//Copy Arrays: Create a copy of one array into another or copy just a specific section.
+//Merge Arrays: Take two sorted arrays and merge them into a single sorted third array.
+//Separate Even and Odd: Scan an array and split its elements into two separate arrays: one for even and one for odd numbers.
