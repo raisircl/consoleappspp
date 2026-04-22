65cc936d8a9395e07e93fd4caa1c2aa61b7fb41d
raisircl <rai.verma@live.com>
2026-04-22 11:27:13 +0530

    constructor

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,12 +3,18 @@ class Program
 {
     static void Main()
     {
-        Rect r1=new Rect();
+        Rect r1=new Rect(); //default constructor   
         r1.Length=10;
         r1.Breadth = 20;
         int a=r1.Area();    
         int p=r1.Perimeter();
         Console.WriteLine($"The area of the rectangle is {a} and the perimeter is {p}.");
+        
+        Rect r2=new Rect(10,20); //parameterized constructor    
+        Console.WriteLine($"R2 dimension is {r2.Length}x{r2.Breadth}");
+
+        Rect r3=new Rect(r2);  //copy constructor   
+        Console.WriteLine($"R3 dimension is {r3.Length}x{r3.Breadth}");
     }
 }
 
@@ -20,5 +26,16 @@ Box: Properties: Length, Breadth, Height
 BankAccount: Properties: AccountNumber, AccountHolderName, Balance
               Behaviors: Deposit, Withdraw, CheckBalance, AccInfo
 
- 
+Distance : Properties: Feet, Inch
+           Behaviors: ToMeter, ToCentimeter, ToYard, Sum of 2 Distances
+
+Distance AddDistance(Distance d)
+{
+        Distance temp=new Distance();
+        temp.Feet=this.Feet + d.Feet;
+        temp.Inch=this.Inch + d.Inch;
+        return temp;    
+}
+Matrix : Properties:  int[,] a=new int[3,3];    
+         Behaviors: Addition, Subtraction
  */
--- ConsoleApp1/Rect.cs
+++ ConsoleApp1/Rect.cs
@@ -7,8 +7,32 @@ namespace ConsoleApp1
    
     public class Rect
     {
+        // Constructor : It is a special method which is used to initialize the object of a class.
+        // It has the same name as the class and it does not have any return type.
+        // it always be public and it is automatically called when we create an object of the class.
+        // it always called once for each object of the class.
+        public Rect()
+        {
+            Length = 1;
+            Breadth = 1;
+        }
+        // parameterized constructor : It is a constructor which takes parameters to initialize the object of a class.
+        public Rect(int l, int b)
+        {
+            Length = l;
+            Breadth = b;
+        }
+        // copy constructor : It is a constructor which takes an object of the same class as a parameter to initialize the object of a class.
+        public Rect(Rect r)
+        {
+            this.Length = r.Length;
+            this.Breadth = r.Breadth;
+        }
+        // this is reference variable which is used to refer the current object of the class.
+
         public int Length { get; set; }
         public int Breadth { get; set; }
+
         public int Area()
         {
             return Length * Breadth;
